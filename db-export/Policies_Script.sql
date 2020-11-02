4e0beee5ffc1c5c23f69c33a93acda080b9ec1d3
Howard Williams <hwilli80@uncc.edu>
2020-11-02 18:36:34 -0500

    Update Policies & Politicians Databases
    Added all Governor and house candidates to politcicians database. Also added more policies to policy database.

--- db-export/Policies_Script.sql
+++ db-export/Policies_Script.sql
@@ -6,4 +6,8 @@ create table policies (
 );
 
 INSERT into policies (Policy)
-values ("Build a wall with Mexico"), ("Get Covid-19 under control"), ("Free College")
+values ("Build a wall with Mexico"), ("Get Covid-19 under control"), ("Free College"),("Legalize Marijuana"),
+("Pro Gay Marriage"),("Anti Gay Marriage"),("Pro Abortion"),("Anti Abortion"),("Pro Confederate Flag"),("Anti Confederate Flag"),
+("Pro Death Penalty"),("Anti Death Penalty"),("Pro Gun COntrol"),("Anti Gun COntrol"),("Pro Gerrymandering"),("Anti Gerrymandering"),("Pro Net Nutrality"),("Anti Net Nutrality"),
+("Pro Immunity for Edward Snowden"),("Anti Immunity for Edward Snowden"),("Pro Obamacare"),("Anti Obamacare"),("Let States decide minimum wage"),("$15 Federal Minimum Wage"),("Supports Corporate Taxes")
+,("Pro COrporate Tax Breaks")
