5be7f0970ca8ceb0d0bba8d13b1cfaab492f403a
Brandon Bost <bbost9@uncc.edu>
2020-12-06 17:10:43 -0500

    updated politician cards

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -5,6 +5,7 @@ class Comparison extends React.Component {
   state = {
     politicians: [],
     image: "https://pbs.twimg.com/profile_images/841429728657580036/_KAJv8es_400x400.jpg",
+    feed: false
   }
 
   update = () => {
@@ -13,21 +14,26 @@ class Comparison extends React.Component {
     })
   }
 
+  flipToFeed = () => {
+    this.setState({feed: !this.state.feed})
+  }
+
   render () {
     console.log("data transfered: ")
     console.log(this.props.politicians);
     const politicians = this.props.politicians;
+
     return(
       <div>
         <div className='comparison'>
           {politicians.length !== 0 && politicians.map((val,i) => (
-            <div key={i} className={`card politician ${val[0].Party}`}>
-              <div>
+            <div key={i} className={`card politician ${val[0].Party}`} onClick={() => this.flipToFeed()}>
+              {this.state.feed ? <Feed keywords={{first: val[0].FirstName, last: val[0].LastName}}/> : <div>
                 <img className="profile" src={this.state.image} alt={val.FirstName}/>
                 <h2>{val[0].FirstName} {val[0].LastName}</h2>
                 <h3>Running Position: {val[0].Running_Position}</h3>
                 <h4>Current Position: {val[0].Current_Position}</h4>
-              </div>
+              </div>}
             </div>
           ))}
         </div>
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 
 class Feed extends React.Component {
   state = {
-    feed: {}
+    feed: [
+      {
+       title: ""
+      }
+    ]
   }
 
   getInitFeed = (subject) => {
@@ -11,18 +15,34 @@ class Feed extends React.Component {
       `http://localhost:4000/newsmedia/${subject}`
     )
     .then(response => {
-      console.log(response)
+      console.log(response.data.articles)
+      this.setState({feed: response.data.articles});
     })
     .catch(err => {
       console.log(err)
     })
+  
   }
 
+  componentDidMount = () => {
+    console.log("feed mounted");
+    console.log(this.state.feed);
+    const search = this.props.keywords.first +' '+ this.props.keywords.last;
+
+    this.getInitFeed(search)
+  }
+
+  componentDidUpdate = () => {
+    console.log("update occured");
+    console.log(this.state.feed);
+  }
 
   render() {
+    console.log(this.props.keywords)
+
     return(
       <div>
-        Test
+        {this.state.feed[0].title}
       </div>
     )
   }
