a1f4d363b7473e4e4d8979857078c479979c9b67
Brandon Bost <bbost9@uncc.edu>
2020-12-17 00:00:05 -0500

    photo link issue

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -34,10 +34,10 @@ class Comparison extends React.Component {
             <div key={i} className={`customcard politician ${val.party} ${this.state.feed &&(this.state.focused === i) ? this.state.focused_styling : this.state.feed ? 'not-focused':''}`}>
               {!this.state.feed ? <Button onClick={() => this.flipToFeed(i)}>Expand Feed</Button>:this.state.focused === i ? <Button onClick={() => this.flipToFeed(i)}>Collapse Feed</Button>:<div/> }
               {this.state.feed ? <NewsFeed keywords={{politician: val, focused: this.state.focused === i ? true: false}}/> : <div>
-                <img className="profile" src={val.photoUrl ||  `../images/${val.party}.svg`} alt={val.name}/>
+                <img className="profile" src={val.photoUrl ||  val.party === 'Democratic Party' ? `https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinclipart.com%2Fpindetail%2FbRmobR_blue-donkey-democrat-donkey-transparent-clipart%2F&psig=AOvVaw3Zq4Z8_W5jD_wWBhjdyT2a&ust=1608267034725000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDHrtyb1O0CFQAAAAAdAAAAABAJ`: `https://www.google.com/url?sa=i&url=https%3A%2F%2Ftoppng.com%2Frepublican-vector-logo-download-free-PNG-free-PNG-Images_467786&psig=AOvVaw0wHj6WBH2qSpdUMgZPdfQc&ust=1608267178293000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMia0qSc1O0CFQAAAAAdAAAAABAQ`} alt={val.name}/>
                 <h2>{val.name}</h2>
                 <h3>{val.party}</h3>
-                <h3>Offical Website: {val.urls ? <a href={val.urls[0]} target='_blank'>{val.urls[0]}</a>: "None Reported"}</h3>
+                <h3>Offical Website: {val.urls ? <a className={val.party === 'Democratic Party' ? 'D-link': ''} href={val.urls[0]} target='_blank'>{val.urls[0]}</a>: "None Reported"}</h3>
               </div>}
             </div>
           ))}
